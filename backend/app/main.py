b8a66f441e8830a2e29c1cac2b4e81e11594138b
Inventrip <inventrip@gmail.com>
2026-06-23 15:58:11 +0530

    initial commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -134,7 +134,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://interview-prapration.vercel.app"
+        "https://interview-model-frontend.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
